ac484bf42df4f122a19e8e851b7518d126e645a3
AndrewKostiv <andrewkostiv1@gmail.com>
2026-09-24 21:54:22 -0400

    added formating and parseData

--- Submittion/submittion.c
+++ Submittion/submittion.c
@@ -1,22 +1,100 @@
-// This program reads an input file of numbers.
-// The input file has on number per line.
-// The program prints out the squares of the input file numbers
-// to the output file.
-// The output file has on number per line.
-
 #include <stdio.h>
 #include <stdlib.h>
 
-long long findNumLines(FILE *fp);
+int findNumLines(FILE *fp);
 
 FILE *srcFP, *destFP;
-long long numLines, *data;
+int numLines, *data;
+
+void merge(int arr[], int left, int mid, int right);
+void mergeSort(int arr[], int left, int right);
+int binarySearch(int n, const int S[], int x);
 
 // ========================
 
-// Merge Sort
+// Main
 
 // ========================
+
+int main(int argc, char *argv[])
+{
+    int i;
+    char str[100];
+    int *b1_input = (int *)malloc(numLines * sizeof(int));
+    int *b2_input = (int *)malloc(numLines * sizeof(int));
+    int *b1_size = 0;
+    int *b2_size = 0;
+
+    if (argc != 3)
+    {
+        printf("Usage: progname <input_file> <output_file>");
+        exit(0);
+    }
+    if ((srcFP = fopen(argv[1], "r")) == NULL)
+    {
+        perror("Error opening input file");
+        exit(0);
+    }
+
+    numLines = findNumLines(srcFP);
+    data = (int *)malloc(numLines * sizeof(int));
+
+    for (i = 0; i < numLines; i++)
+    {
+        fgets(str, 100, srcFP);
+        sscanf(str, "%lld", &(data[i]));
+    }
+
+    if ((destFP = fopen(argv[2], "w")) == NULL)
+    {
+        perror("Error opening output file");
+        exit(0);
+    }
+
+    parseData(data, b1_input, b1_size, b2_input, b2_size);
+
+    writeDestFile(destFP);
+
+    fclose(srcFP);
+    fclose(destFP);
+}
+
+// ========================
+
+// Utility functions
+
+// ========================
+
+int writeDestFile(FILE *fp)
+{
+    int i;
+    for (i = 0; i < numLines; i++)
+        fprintf(fp, "%d\n", data[i] * data[i]);
+}
+
+// returns file size as number of lines in the file
+int findNumLines(FILE *fp)
+{
+
+    int numLines = 0;
+    char str[100];
+
+    while (!feof(fp))
+    {
+        fgets(str, 100, fp);
+        numLines++;
+    }
+
+    numLines--; // the above increments count even when EOF reached
+
+    if (fseek(fp, 0L, SEEK_SET) == EOF)
+    {
+        perror("Error while seeking to begining of file");
+        exit(0);
+    }
+
+    return numLines;
+}
 
 void merge(int arr[], int left, int mid, int right)
 {
@@ -56,12 +134,6 @@ void mergeSort(int arr[], int left, int right)
     }
 }
 
-// ========================
-
-// Binary Search
-
-// ========================
-
 int binarySearch(int n, const int S[], int x)
 {
     int low = 0;
@@ -79,78 +151,23 @@ int binarySearch(int n, const int S[], int x)
     }
     return -1;
 }
-// ========================
 
-// Main
-
-// ========================
-
-int main(int argc, char *argv[])
+void parseData(int *data, int *b1_input, int *b1_size, int *b2_input, int *b2_size)
 {
-
     int i;
-    char str[100];
-
-    if (argc != 3)
-    {
-        printf("Usage: progname <input_file> <output_file>");
-        exit(0);
-    }
-
-    if ((srcFP = fopen(argv[1], "r")) == NULL)
-    {
-        perror("Error opening input file");
-        exit(0);
-    }
-
-    numLines = findNumLines(srcFP);
-
-    data = (long long *)malloc(numLines * sizeof(long long));
-
     for (i = 0; i < numLines; i++)
     {
-        fgets(str, 100, srcFP);
-        sscanf(str, "%lld", &(data[i]));
+        if (data[i] == "//Part B.2")
+        {
+            b1_size = i;
+            break;
+        }
+        b1_input[i] = data[i];
     }
-    if ((destFP = fopen(argv[2], "w")) == NULL)
+
+    for (int j = 0; i < numLines; i++, j++)
     {
-        perror("Error opening output file");
-        exit(0);
+        b2_input[j] = data[i];
     }
 
-    writeDestFile(destFP);
-
-    fclose(srcFP);
-    fclose(destFP);
-}
-
-int writeDestFile(FILE *fp)
-{
-    long long i;
-    for (i = 0; i < numLines; i++)
-        fprintf(fp, "%d\n", data[i] * data[i]);
-}
-
-// returns file size as number of lines in the file
-long long findNumLines(FILE *fp)
-{
-
-    long long numLines = 0;
-    char str[100];
-
-    while (!feof(fp))
-    {
-        fgets(str, 100, fp);
-        numLines++;
-    }
-
-    numLines--; // the above increments count even when EOF reached
-
-    if (fseek(fp, 0L, SEEK_SET) == EOF)
-    {
-        perror("Error while seeking to begining of file");
-        exit(0);
-    }
-
-    return numLines;
 }
